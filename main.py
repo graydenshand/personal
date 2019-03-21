a6f5f619068cee846b05f0d484d4c4a889a3a69d
Grayden Shand <graydenshand@gmail.com>
2019-03-21 19:56:19 -0400

    tutorial templating

--- main.py
+++ main.py
@@ -9,9 +9,15 @@ if 'DYNO' in os.environ: # only trigger SSLify if the app is running on Heroku
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('particles-card.html')
 
+@app.route('/base')
+def base():
+	return render_template('base.html', title="base template")
 
+@app.route('/article')
+def article():
+	return render_template('article.html', title="Command Line Applications", sub_title="Part One: Making Menus")
 
 if __name__ == "__main__":
     app.run()
