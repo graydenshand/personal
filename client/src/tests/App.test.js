98995160845c4d84e6c67927614c3a0ee273e9c8
Grayden <graydenshand@gmail.com>
2022-05-02 17:40:46 -0400

    Fresh start

--- client/src/tests/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from 'App';
-
-test('renders title text', () => {
-  render(<App />);
-  const title = screen.getByText(/Structural Repairs Ongoing/i);
-  expect(title).toBeInTheDocument();
-});
